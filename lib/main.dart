e073b87151fa15eb531dd2ee2374a6cbed59ca88
kal1kidan <kalkidanasdesach@gmail.com>
2026-05-18 16:16:42 -0700

    refactor: clean project structure and optimize codebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,28 @@
 ﻿import 'package:flutter/material.dart';
 import 'package:sizer/sizer.dart';
 
+import 'presentation/home_screen/home_screen.dart';
 import 'routes/app_routes.dart';
 import 'theme/app_theme.dart';
+import 'widgets/custom_error_widget.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  bool hasShownError = false;
+
+  ErrorWidget.builder = (FlutterErrorDetails details) {
+    if (!hasShownError) {
+      hasShownError = true;
+      Future.delayed(const Duration(seconds: 5), () {
+        hasShownError = false;
+      });
+      return CustomErrorWidget(errorDetails: details);
+    }
+
+    return const SizedBox.shrink();
+  };
+
   runApp(const MyApp());
 }
 
