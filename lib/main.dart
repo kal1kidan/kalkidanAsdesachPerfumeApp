32758404252bab5394c31f74a1c741748e6f2ffb
kal1kidan <kalkidanasdesach@gmail.com>
2026-05-18 16:07:24 -0700

    feat: initialize Flutter project structure

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+﻿import 'package:flutter/material.dart';
+import 'package:sizer/sizer.dart';
+
+import 'presentation/home_screen/home_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Sizer(
+      builder: (context, orientation, screenType) {
+        return MaterialApp(
+          title: 'kalperfumes',
+          theme: ThemeData(
+            useMaterial3: true,
+            primarySwatch: Colors.pink,
+          ),
+          home: const HomeScreen(),
+          debugShowCheckedModeBanner: false,
+        );
+      },
+    );
+  }
+}
